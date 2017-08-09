e0e1ec69c6d7780c4a0370b520edc16273ac9aa0
Ganordorf <pascalgimmler@web.de>
2017-08-09 18:51:24 +0200

    Added overview to the interactive IO

--- src/main/InteractiveIO.java
+++ src/main/InteractiveIO.java
@@ -2,17 +2,80 @@ package main;
 
 import java.util.Scanner;
 
+import miscFunctions.Misc;
+
 public class InteractiveIO
 {
 	public InteractiveIO()
 	{
+		Misc.printLogo();
+		System.out.println("Welcome to the interactive IO. Here you can choose between different options and\n"
+				+ "see what is possible. If you want a fast usage of this programm you can start\n"
+				+ "this programm with different Parametes out of the console. For more information\n"
+				+ "start the prorgamm with the parameter '-h' or '-help'.");
 		controller();
 	}
 
 	private void controller()
 	{
+		String choice = null;
+		boolean end = false;
+
+		choice = overview();
+
+		while (!end)
+		{
+			switch (choice)
+			{
+			case "cryptoAlgorithms":
+				Misc.printHeadLine("CRYPTO ALGORITHMS");
+				System.exit(0);
+				break;
+			case "hashAlgorithms":
+				Misc.printHeadLine("HASH ALGORITHMS");
+				System.exit(0);
+				break;
+			case "miscAlgorithms":
+				Misc.printHeadLine("MISC ALGORITHMS");
+				System.exit(0);
+				break;
+			}
+		}
+	}
+
+	private String overview()
+	{
+		String choice = null;
+
+		System.out.println("Choose between following classes of algorithms:\n" + " c - Cryptographic algorithms\n"
+				+ " h - Hash algorithms\n" + " m - Misc algorithms\n" + "Type 'q' to quit this programm");
+
 		Scanner scan = new Scanner(System.in);
 
-		scan.close();
+		while (true)
+		{
+
+			choice = scan.nextLine();
+
+			if (choice.length() > 1)
+			{
+				System.out.println("Wrong input. Please try again.");
+				choice = null;
+			}
+
+			switch (choice)
+			{
+			case "c":
+				scan.close();
+				return "cryptoAlgorithms";
+			case "h":
+				scan.close();
+				return "hashAlgorithms";
+			case "m":
+				scan.close();
+				return "miscAlgorithms";
+			}
+
+		}
 	}
 }
--- src/miscFunctions/Misc.java
+++ src/miscFunctions/Misc.java
@@ -5,18 +5,48 @@ public class Misc
 
 	public static void printHeadLine(String s)
 	{
-		int sLength = s.length() + 2;
 		System.out.println();
-		for (int i = 0; i < 40 - (sLength / 2); i++)
+		
+		if (s != null)
 		{
-			System.out.print("-");
+			int sLength = s.length() + 2;
+			for (int i = 0; i < 40 - (sLength / 2); i++)
+			{
+				System.out.print("-");
+			}
+			System.out.print(" " + s + " ");
+			for (int i = 0; i < 40 - (sLength / 2); i++)
+			{
+				System.out.print("-");
+			}
+		}else {
+			for (int i = 0; i < 80; i++) {
+				System.out.print("-");
+			}
 		}
-		System.out.print(" " + s + " ");
-		for (int i = 0; i < 40 - (sLength / 2); i++)
-		{
-			System.out.print("-");
-		}
-		System.out.println();
 
+		System.out.println();
+	}
+
+	public static void printLogo()
+	{
+		System.out.println();
+		System.out.println("	   _____                  _        _      _ _                          ");
+		System.out.println("	  / ____|                | |      | |    (_) |                         ");
+		System.out.println("	 | |     _ __ _   _ _ __ | |_ ___ | |     _| |__  _ __ __ _ _ __ _   _ ");
+		System.out.println("	 | |    | '__| | | | '_ \\| __/ _ \\| |    | | '_ \\| '__/ _` | '__| | | |");
+		System.out.println("	 | |____| |  | |_| | |_) | || (_) | |____| | |_) | | | (_| | |  | |_| |");
+		System.out.println("	  \\_____|_|   \\__, | .__/ \\__\\___/|______|_|_.__/|_|  \\__,_|_|   \\__, |");
+		System.out.println("	               __/ | |          _        _____       _            __/ |");
+		System.out.println("	              |___/|_|         | |      |  __ \\     | |          |___/ ");
+		System.out.println("	                               | |_ ___ | |  | | ___| |__  _   _  __ _ ");
+		System.out.println("	                               | __/ _ \\| |  | |/ _ \\ '_ \\| | | |/ _` |");
+		System.out.println("	                               | || (_) | |__| |  __/ |_) | |_| | (_| |");
+		System.out.println("	                                \\__\\___/|_____/ \\___|_.__/ \\__,_|\\__, |");
+		System.out.println("	                                                                  __/ |");
+		System.out.println("	                                                                 |___/ ");
+		System.out.println("	                                                         by Leon Becker");
+		System.out.println("	                                                     and Pascal Gimmler");
+		Misc.printHeadLine(null);
 	}
 }
