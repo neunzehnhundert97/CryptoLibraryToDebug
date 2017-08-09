6c2dfb45b59703aaf6eb02bc0d56b48cf88e4605
Leon A. Becker <leonalexbecker@yahoo.de>
2017-08-09 13:37:45 +0200

    Definition of Abtract class HashFunction

---
+++ src/hash/HashFunction.java
@@ -0,0 +1,60 @@
+package hash;
+
+public abstract class HashFunction
+{
+
+	//Constants
+	public static final int QUIET = 0;
+	public static final int INFORMATIVE = 1;
+	public static final int EXCESSIVE = 2;
+	public static final int INHERITIVE = 3;
+	
+	// Attributes
+	private byte[] input;
+	private String output;
+
+	private int verbose;
+
+	// Constructor
+	public HashFunction(byte[] input, int verbose, String output)
+	{
+		//Save parameters
+		this.input = input;
+		this.verbose = verbose;
+		this.output = output;
+		
+		//Print input
+		this.writeOutput(QUIET, "Input ("+input.length+" Bytes)");
+		this.writeOutput(QUIET, input);
+	}
+
+	protected void writeOutput(int verboseLevel, Object... objects)
+	{
+		if (verboseLevel >= this.verbose)
+		{
+			for (Object o : objects)
+			{
+				output += o;
+			}
+			output += '\n';
+		}
+	}
+
+	protected void writeOutput(int verboseLevel, byte[] array)
+	{
+		if (verboseLevel >= this.verbose)
+		{
+			for (int x = 0; x < array.length; ++x)
+			{
+				output += String.format("%02X", array[x]);
+				if (x % 64 == 63)
+					output += "\n\n";
+				else if (x % 16 == 15)
+					output += "\n";
+				else if (x % 4 == 3)
+					output += " ";
+			}
+		}
+	}
+
+}
