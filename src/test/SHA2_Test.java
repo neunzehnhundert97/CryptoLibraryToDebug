35f542ecbe0f18cc32746effdcd7bb56c5240fa3
Leon A. Becker <leonalexbecker@yahoo.de>
2017-08-10 15:20:44 +0200

    Moved SHA_Test into the test package
    And changed verboseLevel to -1

--- src/test/SHA2_Test.java
+++ src/test/SHA2_Test.java
@@ -1,21 +1,22 @@
-package hash;
+package test;
 
 import static org.junit.Assert.*;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import org.junit.Test;
+import hash.SHA2;
 
 public class SHA2_Test
 {
 
-	//@Test
+	@Test
 	public final void testSHA224() throws NoSuchAlgorithmException
 	{
 		System.out.println("Test SHA224");
 		MessageDigest shaCor = MessageDigest.getInstance("SHA-224");
 		
-		for (int size = 500; size < 1000; ++size)
+		for (int size = 0; size < 1000; ++size)
 		{
 			System.out.println("Size: "+size);
 			byte[] input = new byte[size];
@@ -27,19 +28,19 @@ public class SHA2_Test
 					input[i] = (byte) (Math.random()*Byte.MAX_VALUE);
 				}
 				
-				assertArrayEquals(shaCor.digest(input), (new SHA2(input, SHA2.SHA224, 0).digest()));
+				assertArrayEquals(shaCor.digest(input), (new SHA2(input, SHA2.SHA224, -1).digest()));
 				
 			}
 		}
 	}
 	
-	//@Test
+	@Test
 	public final void testSHA256() throws NoSuchAlgorithmException
 	{
 		System.out.println("Test SHA256");
 		MessageDigest shaCor = MessageDigest.getInstance("SHA-256");
 		
-		for (int size = 500; size < 1000; ++size)
+		for (int size = 0; size < 1000; ++size)
 		{
 			System.out.println("Size: "+size);
 			byte[] input = new byte[size];
@@ -51,7 +52,7 @@ public class SHA2_Test
 					input[i] = (byte) (Math.random()*Byte.MAX_VALUE);
 				}
 				
-				assertArrayEquals(shaCor.digest(input), (new SHA2(input, SHA2.SHA256, 0).digest()));
+				assertArrayEquals(shaCor.digest(input), (new SHA2(input, SHA2.SHA256, -1).digest()));
 				
 			}
 		}
@@ -63,7 +64,7 @@ public class SHA2_Test
 		System.out.println("Test SHA384");
 		MessageDigest shaCor = MessageDigest.getInstance("SHA-384");
 		
-		for (int size = 500; size < 1000; ++size)
+		for (int size = 0; size < 1000; ++size)
 		{
 			System.out.println("Size: "+size);
 			byte[] input = new byte[size];
@@ -75,19 +76,19 @@ public class SHA2_Test
 					input[i] = (byte) (Math.random()*Byte.MAX_VALUE);
 				}
 				
-				assertArrayEquals(shaCor.digest(input), (new SHA2(input, SHA2.SHA384, 0).digest()));
+				assertArrayEquals(shaCor.digest(input), (new SHA2(input, SHA2.SHA384, -1).digest()));
 				
 			}
 		}
 	}
 	
-	//@Test
+	@Test
 	public final void testSHA512() throws NoSuchAlgorithmException
 	{
 		System.out.println("Test SHA512");
 		MessageDigest shaCor = MessageDigest.getInstance("SHA-512");
 		
-		for (int size = 500; size < 1000; ++size)
+		for (int size = 0; size < 1000; ++size)
 		{
 			System.out.println("Size: "+size);
 			byte[] input = new byte[size];
@@ -99,7 +100,7 @@ public class SHA2_Test
 					input[i] = (byte) (Math.random()*Byte.MAX_VALUE);
 				}
 				
-				assertArrayEquals(shaCor.digest(input), (new SHA2(input, SHA2.SHA512, 0).digest()));
+				assertArrayEquals(shaCor.digest(input), (new SHA2(input, SHA2.SHA512, -1).digest()));
 				
 			}
 		}
